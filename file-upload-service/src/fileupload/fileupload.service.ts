1a91d8a695b15d2ae68f2b6c770b06c01791c95c
virangaj <virangapasindu4@gmail.com>
2025-01-13 02:34:20 +0530

    upload file completed

--- file-upload-service/src/fileupload/fileupload.service.ts
+++ file-upload-service/src/fileupload/fileupload.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, Logger } from '@nestjs/common';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as XLSX from 'xlsx';
 @Injectable()
-export class FileuploadService {}
+export class FileuploadService {
+  private readonly logger = new Logger(FileuploadService.name, {
+    timestamp: true,
+  });
+  processBulkUplod(filename: string) {
+    // create the filepath
+    const filePath = path.join(
+      __dirname.replace('dist', ''),
+      `../uploads/${filename}`,
+    );
+    this.logger.log('Found file in : ' + filePath);
+
+    try {
+      //  naviagate to file in local file path adn open the file
+      //  find the data from sheet
+      const workbook = XLSX.readFile(filePath);
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      const data = XLSX.utils.sheet_to_json(worksheet, { raw: true });
+      return data;
+    } catch (err) {
+      this.logger.error('Failed to parse file : ' + err.message);
+      return null;
+    } finally {
+      //  delete the file from localstorage
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          this.logger.error('Failed to delete file : ' + err.message);
+        } else {
+          this.logger.log('Deleted file : ' + filePath);
+        }
+      });
+    }
+  }
+}

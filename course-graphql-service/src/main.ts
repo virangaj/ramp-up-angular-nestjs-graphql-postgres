e793bf07b6673c9647937efaa347192e8cc0c7e6
virangaj <virangapasindu4@gmail.com>
2025-02-07 12:38:28 +0530

    remove courses

--- course-graphql-service/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3003);
-  console.log(`File upload server is running on: ${await app.getUrl()}`);
-}
-bootstrap();
